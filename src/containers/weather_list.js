55798ac5b4c4ae54d5dc8cbeada35f432fda9cfa
german <gpereira@travelit.com.ar>
2017-10-29 22:22:58 -0300

    se muestra grafica de temperatura, humedad y presion

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import Chart from "../components/chart";
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const presures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Chart data={temps} color="orange" />
+        </td>
+        <td>
+          <Chart data={presures} color="red" />
+        </td>
+        <td>
+          <Chart data={humidities} color="blue" />
+        </td>
       </tr>
     );
   }
@@ -18,7 +31,7 @@ class WeatherList extends Component {
           <tr>
             <th>City</th>
             <th>Temperature</th>
-            <th>Preassure</th>
+            <th>Pressure</th>
             <th>Humidity</th>
           </tr>
         </thead>
