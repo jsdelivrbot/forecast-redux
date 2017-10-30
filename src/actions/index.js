0733a5ef8e1889c2b97af0ed9a9139ca68c44c12
german <gpereira@travelit.com.ar>
2017-10-29 21:03:48 -0300

    instalo axios y key de weather

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const API_KEY = "015a0582a92737bdfc7028cc0c7f18a6";
+export const ROOT_URL = `http://openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
