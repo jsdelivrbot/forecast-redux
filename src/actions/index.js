42f2f1cc627ca893c604a947addf0d3505dfe0f1
german <gpereira@travelit.com.ar>
2017-10-29 21:18:50 -0300

    llamada a openweather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API_KEY = "015a0582a92737bdfc7028cc0c7f18a6";
-export const ROOT_URL = `http://openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
 export function fetchWeather(city) {
