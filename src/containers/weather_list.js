cd339e49826f9dc6f626c3accf168eb70d800101
german <gpereira@travelit.com.ar>
2017-10-29 22:43:20 -0300

    se muestra mapa de la ciudad ingresada

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,24 +1,29 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Chart from "../components/chart";
+import GoogleMap from "../components/google_map";
+
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
     const presures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
 
     return (
       <tr key={name}>
-        <td>{name}</td>
         <td>
-          <Chart data={temps} color="orange" />
+          <GoogleMap lon={lon} lat={lat} />
         </td>
         <td>
-          <Chart data={presures} color="red" />
+          <Chart data={temps} color="orange" units="K" />
         </td>
         <td>
-          <Chart data={humidities} color="blue" />
+          <Chart data={presures} color="red" units="hPa" />
+        </td>
+        <td>
+          <Chart data={humidities} color="blue" units="%" />
         </td>
       </tr>
     );
@@ -30,8 +35,8 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
             <th>Humidity</th>
           </tr>
         </thead>
